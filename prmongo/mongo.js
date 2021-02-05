f52c9c14cc7464928a988c4d3dc63337a90a36f5
asif <mdasifr28@gmail.com>
2021-02-06 03:58:36 +0530

    MongoDB initial commit

---
+++ prmongo/mongo.js
@@ -0,0 +1,27 @@
+const { MongoClient } = require("mongodb");
+import bookData from './books.json';
+
+const uri ="mongodb://localhost:27017"
+const client = new MongoClient(uri,{ useUnifiedTopology: true });
+
+async function run() {
+  try {
+    await client.connect();
+    // const database = client.db('cv_raman');
+    // const collection = database.collection('collection');
+
+    const database = client.db('cv_raman');
+    const collection = database.collection('books');
+  
+    // const query = { "name":"baby" };
+    // const result = await collection.findOne(query);
+
+    const result = await collection.insertMany(bookData);
+
+    console.log(result);
+  } finally {
+ 
+    await client.close();
+  }
+}
+run().catch(console.dir);
