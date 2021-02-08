198047d1286c08227cf66532ca93bad7537f0aa4
asif <mdasifr28@gmail.com>
2021-02-08 16:51:39 +0530

    error in mongo.js file solved

--- prmongo/mongo.js
+++ prmongo/mongo.js
@@ -1,23 +1,19 @@
-import bookData from './books.json';
-const { MongoClient } = require("mongodb");
+import {data} from './books.js';
+import pkg from 'mongodb';
+import fs from 'fs';
+const { MongoClient } = pkg;
 
 const uri ="mongodb://localhost:27017"
 const client = new MongoClient(uri,{ useUnifiedTopology: true });
 
+
 async function run() {
   try {
     await client.connect();
-    // const database = client.db('cv_raman');
-    // const collection = database.collection('collection');
-
     const database = client.db('cv_raman');
-    const collection = database.collection('books');
-  
-    // const query = { "name":"baby" };
-    // const result = await collection.findOne(query);
-
-    const result = await collection.insertMany(bookData);
-
+    const collection = database.collection('collection');
+    const query = { "name":"Asif" };
+    const result = await collection.findOne(query);
     console.log(result);
   } finally {
  
@@ -25,3 +21,16 @@ async function run() {
   }
 }
 run().catch(console.dir);
+
+async function bookd() {
+  try {
+    await client.connect();
+    const database = client.db('cv_raman');
+    const collection = database.collection('books');
+    const result = await collection.insertMany(data);
+    console.log(result);
+  } finally {
+    await client.close();
+  }
+}
+bookd().catch(console.dir);
