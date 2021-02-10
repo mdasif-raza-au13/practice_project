dcadb41228c407ebc89a71a729fa117b2b50dff4
asif <mdasifr28@gmail.com>
2021-02-10 21:55:04 +0530

    Simplified the index file

--- pr1/src/index.js
+++ pr1/src/index.js
@@ -1,53 +1,17 @@
 import express from 'express';
 import bodyparser from 'body-parser';
-import { v4 as uuidv4 } from 'uuid';
+import userRoutes from './routes/users.js';
+
 const app = express();
 const PORT = 5050;
-const data = [];
 
 app.use(bodyparser.json());
+app.use('/api', userRoutes);
 
-app.post('/app/v1/todo/create',(req,res)=>{
-    const dt = req.body;
-    data.push({...dt, id : uuidv4()});
-    console.log(req.body);
-    res.json({message:"enter the following id, body, iscompleted, createat"});
-});
+app.get("/", (req, res) => res.send("Welcome to the Users API!"));
 
-app.get('/app/v1/todo/all',(req,res)=>{
-    console.log(data);
-    const message = "Showing details";
-    res.json({
-        data: data,
-        message
-    })
-});
-
-app.put('app/v1/todo/:id',(req,res)=>{
-    let userId = data.filter(user => user.id == req.params.id);
-    const message = "User updated";
-    if(userId.length){
-        data = data.map(user =>{
-            if(user.id == req.params.id){
-                userId = User({
-                    id : user.id,
-                    ...req.body
-                })
-                return userId
-            }
-            return user
-        });
-    }else{
-        userId = User({id: uuidv4(), ...req.body});
-        users.push(userId);
-        message = 'New user created.';
-    }
-    res.status(201).json({
-        data: userId,
-        message
-    })
-});
+app.all("*", (req, res) =>res.send("You've tried reaching a route that doesn't exist."));
 
 app.listen(PORT,()=>{
-    console.log(`listening on http://localhost:${PORT}/app/v1/todo/all`);
+    console.log(`listening on http://localhost:${PORT}/api/all`);
 });
